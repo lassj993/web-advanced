81648ba232cd536a02e4634c8f15bb2d61554c5a
lassj993 <31547374+lassj993@users.noreply.github.com>
2017-09-15 11:48:15 -0400

    Add files via upload

---
+++ hidden-pattern/main.js
@@ -0,0 +1,191 @@
+console.log("hi")
+
+var array1 = ["/////////////////////", "/////////////////////////", "//////////////////////////", "////////////////////", "/////////////////////////////////////////////////////"],
+    array2 = ["hi", "who", "are", "you", "why are you here"],
+    array3 = ["\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\", "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\", "...............................................", "ooooooooooooooooooooooooooooooooooooooo", "what the fuck"],
+    array4 = ["///////////////////////////////////////", "+++++++++++++++++++++++++++++++++++++++++" , "gfdgfdfddgfdgfdgfdgdf", "99999999999999999999999999999999", "if you search in the forest occassionally you'll find an arrowhead"];
+
+for (var i = 0; i < array1.length; i++) {
+
+    if (array1[i].length < array4[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array4.length;
+		randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array4[randomQuoteIndex];
+
+    } else {
+    	var randomQuoteIndexx = Math.random() * array1.length;
+
+		
+    } 
+
+}
+
+
+for (var i = 0; i < array1.length; i++) {
+
+    if (array2[i].length > array3[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array3.length;
+		randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array3[randomQuoteIndex];
+
+
+    } else {
+    	var randomQuoteIndexx = Math.random() * array2.length;
+    	randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array2[randomQuoteIndex];
+		
+    } 
+
+}
+
+for (var i = 0; i < array1.length; i++) {
+
+    if (array2[i].length < array3[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array3.length;
+		randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array3[randomQuoteIndex];
+
+    } else {
+    	var randomQuoteIndexx = Math.random() * array2.length;
+		
+    } 
+
+}
+
+for (var i = 0; i < array1.length; i++) {
+
+    if (array1[i].length < array4[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array4.length;
+		randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array1[randomQuoteIndex];
+
+    } else {
+    	var randomQuoteIndexx = Math.random() * array1.length;
+
+		
+    } 
+
+}
+
+for (var i = 0; i < array1.length; i++) {
+
+    if (array2[i].length < array3[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array3.length;
+		randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array3[randomQuoteIndex];
+
+
+    } else {
+    	var randomQuoteIndexx = Math.random() * array2.length;
+    	randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array2[randomQuoteIndex];
+		
+    } 
+
+}
+
+for (var i = 0; i < array1.length; i++) {
+
+    if (array1[i].length < array4[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array4.length;
+		randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array1[randomQuoteIndex];
+
+    } else {
+    	var randomQuoteIndexx = Math.random() * array4.length;
+
+		
+    } 
+
+}
+
+
+for (var i = 0; i < array1.length; i++) {
+
+    if (array2[i].length > array3[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array3.length;
+		randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array3[randomQuoteIndex];
+
+
+    } else {
+    	var randomQuoteIndexx = Math.random() * array2.length;
+    	randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array2[randomQuoteIndex];
+		
+    } 
+
+}
+
+for (var i = 0; i < array1.length; i++) {
+
+    if (array2[i].length < array3[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array3.length;
+		randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array3[randomQuoteIndex];
+
+    } else {
+    	var randomQuoteIndexx = Math.random() * array2.length;
+		
+    } 
+
+}
+for (var i = 0; i < array1.length; i++) {
+
+    if (array1[i].length < array4[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array4.length;
+		randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array4[randomQuoteIndex];
+
+    } else {
+    	var randomQuoteIndexx = Math.random() * array1.length;
+
+		
+    } 
+
+}
+
+
+
+for (var i = 0; i < array1.length; i++) {
+
+    if (array2[i].length < array3[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array3.length;
+		randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array3[randomQuoteIndex];
+
+
+    } else {
+    	var randomQuoteIndexx = Math.random() * array2.length;
+    	randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array2[randomQuoteIndex];
+		
+    } 
+
+}
+
+
