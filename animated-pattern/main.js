7bdaad4be2c6f2be9e782c09315d95d5b0e87c05
lassj993 <31547374+lassj993@users.noreply.github.com>
2017-09-22 10:56:11 -0400

    Add files via upload

---
+++ animated-pattern/main.js
@@ -0,0 +1,323 @@
+var snowTimer = 0; //declare our timer for setTimeout
+
+
+for (var i = 0; i < 3; i++) { //standard for loop to iterate for 200 console lines
+    setTimeout(function() { // introduce the setTimout function
+    var array1 = ["/////////////////////", "/////////////////////////", "//////////////////////////", "////////////////////", "/////////////////////////////////////////////////////"],
+    array2 = ["hi", "who", "are", "you", "why are you here"],
+    array3 = ["\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\", "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\", "...............................................", "excuse me who are you", "what the fuck"],
+    array4 = ["///////////////////////////////////////", "////////////////////////////////" , "||||||||||||||||||||||||||||||", "{{{{{{{{{{{{}}}}}}}{{{{}}}}}}}}}", "hey what are you doing here"];
+
+for (var i = 0; i < array1.length; i++) {
+
+    if (array1[i].length < array4[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array4.length;
+        randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array4[randomQuoteIndex];
+
+    } else {
+        var randomQuoteIndexx = Math.random() * array1.length;
+
+        
+    } 
+
+}
+
+for (var i = 0; i < array1.length; i++) {
+
+    if (array2[i].length < array3[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array3.length;
+        randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array3[randomQuoteIndex];
+
+    } else {
+        var randomQuoteIndexx = Math.random() * array2.length;
+        
+    } 
+
+}
+for (var i = 0; i < array1.length; i++) {
+
+    if (array1[i].length < array4[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array4.length;
+        randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array4[randomQuoteIndex];
+
+    } else {
+        var randomQuoteIndexx = Math.random() * array1.length;
+
+        
+    } 
+
+}
+
+
+
+for (var i = 0; i < array1.length; i++) {
+
+    if (array2[i].length < array3[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array3.length;
+        randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array3[randomQuoteIndex];
+
+
+    } else {
+        var randomQuoteIndexx = Math.random() * array2.length;
+        randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array2[randomQuoteIndex];
+        
+    } 
+
+}
+
+        // var snow = ""; //empty string that will be filled below!
+
+
+        // for (var s = 0; s < 40; s++) { //for loop stating 40 characters per line
+
+        //     var snowOrRain = Math.random()*100; 
+        //     // use snowOrRain to determine what combination of ❄, 💧, and " " each line has
+        //     if (snowOrRain < 33) {
+        //         snow += "❄️"; //add snow to string
+
+        //     }else if (snowOrRain > 33 && snowOrRain < 66){
+        //         snow += "💧"; //add rain to string
+        //     }else {
+        //         snow += " "; //add space to string
+        //     }
+
+        // }
+        // console.log(snow); //output of snow string
+    }, snowTimer); // snowTimer determines when the sleet string is released
+    snowTimer += 200; // in order to "animate", each sleet string is released +70 milliseconds after the previous one.
+} 
+
+for (var i = 0; i < 10; i++) { //standard for loop to iterate for 200 console lines
+    setTimeout(function() { // introduce the setTimout function
+    var array1 = ["/////////////////////////////////////////////////////", "/////////////////////////////////////////////////////", "/////////////////////////////////////////////", "//////////////////////////////////////", "/////////////////////////////////////////////////////"],
+    array2 = ["hi", "who", "are", "you", "why are you here"],
+    array3 = ["\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\", "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\", "...............................................", "excuse me who are you", "what the fuck"],
+    array4 = ["///////////////////////////////////////", "////////////////////////////////" , "||||||||||||||||||||||||||||||", "{{{{{{{{{{{{}}}}}}}{{{{}}}}}}}}}", "hey what are you doing here"];
+
+for (var i = 0; i < array1.length; i++) {
+
+    if (array1[i].length > array4[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array1.length;
+        randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array1[randomQuoteIndex];
+
+    } else {
+        var randomQuoteIndexx = Math.random() * array4.length;
+
+        
+    } 
+
+}
+
+for (var i = 0; i < array1.length; i++) {
+
+    if (array2[i].length > array3[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array3.length;
+        randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array3[randomQuoteIndex];
+
+    } else {
+        var randomQuoteIndexx = Math.random() * array2.length;
+        randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array2[randomQuoteIndex];
+        
+    } 
+
+}
+for (var i = 0; i < array1.length; i++) {
+
+    if (array1[i].length < array4[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array4.length;
+        randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array4[randomQuoteIndex];
+
+    } else {
+        var randomQuoteIndexx = Math.random() * array1.length;
+
+        
+    } 
+
+}
+
+
+
+for (var i = 0; i < array1.length; i++) {
+
+    if (array2[i].length < array3[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array3.length;
+        randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array3[randomQuoteIndex];
+
+
+    } else {
+        var randomQuoteIndexx = Math.random() * array2.length;
+        randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array2[randomQuoteIndex];
+        
+    } 
+
+}
+
+        // var snow = ""; //empty string that will be filled below!
+
+
+        // for (var s = 0; s < 40; s++) { //for loop stating 40 characters per line
+
+        //     var snowOrRain = Math.random()*100; 
+        //     // use snowOrRain to determine what combination of ❄, 💧, and " " each line has
+        //     if (snowOrRain < 33) {
+        //         snow += "❄️"; //add snow to string
+
+        //     }else if (snowOrRain > 33 && snowOrRain < 66){
+        //         snow += "💧"; //add rain to string
+        //     }else {
+        //         snow += " "; //add space to string
+        //     }
+
+        // }
+        // console.log(snow); //output of snow string
+    }, snowTimer); // snowTimer determines when the sleet string is released
+    snowTimer += 100; // in order to "animate", each sleet string is released +70 milliseconds after the previous one.
+} 
+
+for (var i = 0; i < 105; i++) { //standard for loop to iterate for 200 console lines
+    setTimeout(function() { // introduce the setTimout function
+    var array1 = ["///////////////////////////////////////////////////////////////", "////////////////////////////////////////////////////////", "////////////////////////////////////////////////////////////", "/////////////////////////////////////////////////", "////////////////////////////////////////////////////////////////////////////"],
+    array2 = ["hi", "who", "are", "you", "why are you here"],
+    array3 = ["\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\", "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\", "...............................................", "excuse me who are you", "what the fuck"],
+    array4 = ["///////////////////////////////////////", "////////////////////////////////" , "||||||||||||||||||||||||||||||", "{{{{{{{{{{{{}}}}}}}{{{{}}}}}}}}}", "hey what are you doing here"];
+
+for (var i = 0; i < array1.length; i++) {
+
+    if (array1[i].length > array4[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array1.length;
+        randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array1[randomQuoteIndex];
+
+    } 
+}
+for (var i = 0; i < array1.length; i++) {
+
+    if (array2[i].length < array3[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array3.length;
+        randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array3[randomQuoteIndex];
+
+    } else {
+        var randomQuoteIndexx = Math.random() * array2.length;
+        
+    } 
+
+}
+
+
+        // var snow = ""; //empty string that will be filled below!
+
+
+        // for (var s = 0; s < 40; s++) { //for loop stating 40 characters per line
+
+        //     var snowOrRain = Math.random()*100; 
+        //     // use snowOrRain to determine what combination of ❄, 💧, and " " each line has
+        //     if (snowOrRain < 33) {
+        //         snow += "❄️"; //add snow to string
+
+        //     }else if (snowOrRain > 33 && snowOrRain < 66){
+        //         snow += "💧"; //add rain to string
+        //     }else {
+        //         snow += " "; //add space to string
+        //     }
+
+        // }
+        // console.log(snow); //output of snow string
+    }, snowTimer); // snowTimer determines when the sleet string is released
+    snowTimer += 60; // in order to "animate", each sleet string is released +70 milliseconds after the previous one.
+} 
+
+
+
+for (var i = 0; i < 200; i++) { //standard for loop to iterate for 200 console lines
+    setTimeout(function() { // introduce the setTimout function
+    var array1 = ["///////////////////////////////////////////////////////////////////////////////", "////////////////////////////////////////////////////////////////////////", "////////////////////////////////////////////////////////////////////////////", "/////////////////////////////////////////////////", "////////////////////////////////////////////////////////////////////////////////////////////"],
+    array2 = ["hi", "who", "are", "you", "why are you here"],
+    array3 = ["\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\", "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\", "...............................................", "excuse me who are you", "what the fuck"],
+    array4 = ["///////////////////////////////////////", "////////////////////////////////" , "||||||||||||||||||||||||||||||", "{{{{{{{{{{{{}}}}}}}{{{{}}}}}}}}}", "hey what are you doing here"];
+
+for (var i = 0; i < array1.length; i++) {
+
+    if (array1[i].length > array4[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array1.length;
+        randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array1[randomQuoteIndex];
+
+    } 
+}
+for (var i = 0; i < array1.length; i++) {
+
+    if (array2[i].length < array3[i].length) { 
+
+        var randomQuoteIndex = Math.random() * array3.length;
+        randomQuoteIndex = Math.floor(randomQuoteIndex);
+        console.log(randomQuote);
+        var randomQuote = array3[randomQuoteIndex];
+
+    } else {
+        var randomQuoteIndexx = Math.random() * array2.length;
+        
+    } 
+
+}
+
+
+        // var snow = ""; //empty string that will be filled below!
+
+
+        // for (var s = 0; s < 40; s++) { //for loop stating 40 characters per line
+
+        //     var snowOrRain = Math.random()*100; 
+        //     // use snowOrRain to determine what combination of ❄, 💧, and " " each line has
+        //     if (snowOrRain < 33) {
+        //         snow += "❄️"; //add snow to string
+
+        //     }else if (snowOrRain > 33 && snowOrRain < 66){
+        //         snow += "💧"; //add rain to string
+        //     }else {
+        //         snow += " "; //add space to string
+        //     }
+
+        // }
+        // console.log(snow); //output of snow string
+    }, snowTimer); // snowTimer determines when the sleet string is released
+    snowTimer += 25; // in order to "animate", each sleet string is released +70 milliseconds after the previous one.
+} 
+
+
